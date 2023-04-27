366d49aff65506bf7b15d6b8f33773660b84212d
Dr.Vegapunk <doctorvegapunk@gmail.com>
2023-04-27 06:18:02 +0300

    J

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 const express  = require('express')
 const puppeteer = require("puppeteer")
-require("dotenv").config()
 
-const port  = process.env.PORT || 5000
+const port  = process.env.PORT || 3000
 const app = express()
 
 app.get('/',async(req,res)=>{
     const browser = await puppeteer.launch({
-        args:[
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--single-process',
-            '--no-zygote'
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
         ],
-        executablePath: process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath()
-    })
-    const page = await browser.newPage()
-    await page.goto('https://mangareader.to/')
-
-    const testText = await page.$$("div.social-home-block.mb-4 >div.shb-left")
-    const innerHTML = await testText[0].evaluate(element => element.innerHTML)
-    res.send(innerHTML)
-    await browser.close()
+      });
+      const page = await browser.newPage();
+      await page.goto('https://example.com');
+      await page.screenshot({ path: 'example.png' });
+      res.send("Okey")
+      await browser.close();
 })
+app.get('/hoop',(req,res)=>{
 
+    console.log("d");
+    res.send("Dd")
+})
 
 app.listen(port, ()=> console.log(`Server listening on port: ${port}`))
 
